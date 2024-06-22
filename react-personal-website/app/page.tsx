cdbb168f471f4daed4e02a57ab335ab64b8412c9
Bobby Boland <rboland01@gmail.com>
2024-06-22 19:14:02 -0400

    Updated Nav Bar

--- react-personal-website/app/page.tsx
+++ react-personal-website/app/page.tsx
@@ -2,7 +2,48 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col">
+      <div className="bg-white">
+        <nav className="flex max-w-full p-5 justify-between">
+          <div className="lg:flex lg:gap-x-12">
+            <div>
+            <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className="dark:invert"
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+            </div>
+            <div>
+             <a href="#" className="text-sm font-semibold leading-6">Features</a>
+            </div>
+            <div>
+              <a href="#" className="text-sm font-semibold leading-6">Marketplace</a>
+            </div>
+            <div>
+             <a href="#" className="text-sm font-semibold leading-6">Company</a>
+            </div>
+            <div>
+             <a href="#" className="text-sm font-semibold leading-6">Features</a>
+            </div>
+            <div>
+              <a href="#" className="text-sm font-semibold leading-6">Marketplace</a>
+            </div>
+            <div>
+             <a href="#" className="text-sm font-semibold leading-6">Company</a>
+            </div>
+          </div>
+        </nav>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
